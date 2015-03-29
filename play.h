9b2225493c3441fe15f552050e8abd034dd4b5db
Jacob White <whi10021@byui.edu>
2015-03-28 20:07:00 -0600

    Add ability to turn to desired direction while moving to point

--- play.h
+++ play.h
@@ -1,6 +1,6 @@
 #include "skill.h"
 
-float play_rushGoal(coord3 robot, coord2 ball)
+void play_rushGoal(coord3 robot, coord2 ball)
 {
 	// normal vector from ball to goal
 	coord2 n = utility_unitVector(utility_getVector(ball, P_GOAL));
@@ -8,30 +8,34 @@ float play_rushGoal(coord3 robot, coord2 ball)
 	// compute position 10cm behind ball, but aligned with goal.
 	coord2 position = utility_addVector(ball, utility_multVector(n, -10));
 
+	float goalAngle = utility_getAngle(utility_3to2(robot1currentPosition), P_GOAL);
 	if(utility_dist(position, utility_3to2(robot)) < 20) //10.5
 	{
 		//printf("Go to goal\n");
-		return skill_goToPoint(robot, P_GOAL);
+		skill_goToPoint(robot, (coord3){P_GOAL.x, P_GOAL.y, goalAngle});
 	}
 	else
 	{
-		//printf("Go behind ball\n");
-		return skill_goToPoint(robot, position);
+//		//printf("Go behind ball\n");
+//		if (fabs(robot.y - ball.y) > 10)
+			skill_goToPoint(robot, (coord3){position.x, position.y, goalAngle});
+//		else
+//		{
+//			if(robot.y > ball.y)
+//				skill_goToPoint(robot, (coord3){position.x - 10, position.y + 10, 0});
+//			else
+//				skill_goToPoint(robot, (coord3){position.x - 10, position.y - 10, 0});
+//		}
 	}
 }
 
-float play_followBallOnLine(coord3 robot, coord2 ball, float xPos)
+void play_blockGoal(coord3 robot, coord2 ball)
 {
-	 return skill_goToPoint(robot, (coord2){xPos, ball.y});
-/*	// control x position to stay on current line
-	 float vx = -P_CONTROL_K_XY*(robot.x - xPos);
+	coord3 point = (coord3){-P_GOAL.x + 20, ball.y, 0};
+	if(point.y > P_GOAL.y + P_GOAL_WIDTH / 2)
+		point.y = P_GOAL.y + P_GOAL_WIDTH / 2;
+	if(point.y < P_GOAL.y - P_GOAL_WIDTH / 2)
+		point.y = P_GOAL.y - P_GOAL_WIDTH / 2;
 
-	 // control y position to match the ball's y-position
-	 float vy = -P_CONTROL_K_XY*(robot.y - ball.y);
-
-	 // control angle to -pi/2
-	 float theta_d = utility_getAngle(utility_3to2(robot), P_GOAL);
-	 float omega = -P_CONTROL_K_W*(robot.w - theta_d);
-
-	moveRobotWorldCoordinates(robot, (coord3){vx, vy, omega});*/
+	skill_goToPoint(robot, point);
 }
--- skill.h
+++ skill.h
@@ -4,19 +4,27 @@
 //                    Skills
 //######################################################################
 
-float skill_goToPoint(coord3 robot, coord2 point)
+void skill_goToPoint(coord3 robot, coord3 point)
 {
-	coord2 velocity = utility_multVector(utility_getVector(utility_3to2(robot), point), P_CONTROL_K_XY);
+	coord2 diff = utility_getVector(utility_3to2(robot), utility_3to2(point));
+	coord2 velocity = utility_multVector(diff, P_CONTROL_K_XY);
+
 	if(utility_dist1(velocity) > P_MAX_VELOCITY)
 		velocity = utility_vectorWithLength(velocity, P_MAX_VELOCITY);
 
 	// face goal
-	float theta_d = utility_getAngle(utility_3to2(robot), P_GOAL);
-	float omega = -P_CONTROL_K_W*(robot.w - theta_d);
+	float omega = -P_CONTROL_K_W * utility_angleMod(robot.w - point.w);
+	if(fabs(omega) > P_MAX_SPIN)
+		omega = fsign(omega) * P_MAX_SPIN;
 
-	moveRobotWorldCoordinates(robot, (coord3){velocity.x, velocity.y, 0}); //omega
-	//P_MAX_SPIN
-	return utility_dist(utility_3to2(robot), point);
+	if(DEBUG_PRINT)
+	{
+		printf("Angle: %f goal: %f omega: %f\n", robot1currentPosition.w, point.w, omega);
+
+		//printf("distance: %f, velocity: %f, omega: %f\n", utility_dist1(diff), utility_dist1(velocity), omega);
+	}
+
+	moveRobotWorldCoordinates(robot, (coord3){velocity.x, velocity.y, omega});
 }
 
 
