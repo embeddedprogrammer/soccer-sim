4e9d5d7cacd726ca7147ce741bd48b8ad7719246
Jacob White <jacob.white@byu.net>
2016-02-23 15:03:29 -0700

    Plugin now subscribes to topic.

--- plugins/model_push.cc
+++ plugins/model_push.cc
@@ -3,34 +3,53 @@
 #include <gazebo/physics/physics.hh>
 #include <gazebo/common/common.hh>
 #include <stdio.h>
+#include "ros/ros.h"
+#include "std_msgs/Float64.h"
 
 namespace gazebo
 {
   class ModelPush : public ModelPlugin
   {
-    public: void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/)
+  public: 
+    void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/)
     {
       // Store the pointer to the model
       this->model = _parent;
 
+      /*
+       * Connect the Plugin to the Robot and Save pointers to the various elements in the simulation
+       */
+      // if (_sdf->HasElement("namespace"))
+      //   namespace_ = _sdf->GetElement("namespace")->Get<std::string>();
+      // else
+      //   gzerr << "[gazebo_aircraft_forces_and_moments] Please specify a namespace.\n";
+      // node_handle_ = new ros::NodeHandle(namespace_);
+      node_handle = ros::NodeHandle("robot");
+      command_sub = node_handle.subscribe("/command", 1, &ModelPush::CommandCallback, this);
+
       // Listen to the update event. This event is broadcast every
       // simulation iteration.
-      this->updateConnection = event::Events::ConnectWorldUpdateBegin(
-          boost::bind(&ModelPush::OnUpdate, this, _1));
+      this->updateConnection = event::Events::ConnectWorldUpdateBegin(boost::bind(&ModelPush::OnUpdate, this, _1));
     }
 
     // Called by the world update start event
-    public: void OnUpdate(const common::UpdateInfo & /*_info*/)
+    void OnUpdate(const common::UpdateInfo & /*_info*/)
     {
       // Apply a small linear velocity to the model.
       this->model->SetLinearVel(math::Vector3(-.1, 0, 0));
     }
 
-    // Pointer to the model
-    private: physics::ModelPtr model;
+    void CommandCallback(const std_msgs::Float64 msg)
+    {
+      ROS_INFO("Hello World %f", msg.data);
+    }
 
-    // Pointer to the update event connection
-    private: event::ConnectionPtr updateConnection;
+  private: 
+    // Pointer to the model
+    physics::ModelPtr model;
+    event::ConnectionPtr updateConnection;
+    ros::NodeHandle node_handle;
+    ros::Subscriber command_sub;
   };
 
   // Register this plugin with the simulator
